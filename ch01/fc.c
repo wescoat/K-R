e586a126725c0199204ad070862b82285b588e01
Michael Wescoat <michael.t.wescoat@gmail.com>
2020-06-29 14:32:57 -0700

    improved output of fc

--- ch01/fc.c
+++ ch01/fc.c
@@ -4,7 +4,7 @@
     for fahr = 0, 20, ..., 300 */
 main()
 {
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0;      /* lower limit of temperature table */
@@ -13,8 +13,8 @@ main()
 
     fahr = lower;
     while (fahr <= upper) {
-        celsius = 5 * (fahr-32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
